331cf70a6f7325d51239b73791f577fd24421ffa
unknown <pwhynotfilms@gmail.com>
2019-05-21 19:22:51 -0500

    Port connected

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 //Dependcies
-
+const express = require('express');
+const path = require('path');
 // Sets up the Express App
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 //Sets up the Express app to handle data parsing
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 
@@ -11,7 +14,12 @@
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-
+app.get("/", function(reg, res) {
+    res.sendFile(path.join(__dirname, 'index.html' ));
+});
 
 
 // Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
