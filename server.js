8528e1452b87ef20a5e2349ec42c178f6ced1907
unknown <pwhynotfilms@gmail.com>
2019-05-21 21:28:13 -0500

    javascript started on tables.html

--- server.js
+++ server.js
@@ -25,26 +25,55 @@ const reservations = [
  }
 
 ];
-//AJAX function that uses URL to reference API to GET 
-
 
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(reg, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html" ));
 });
 
-app.get("/tables", function(reg, res) {
+app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html" ));
 });
 
-app.get("/reserve", function(reg, res) {
-    res.sendFile(path.join(__dirname, "reserve.html" ));
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "./reserve.html" ));
 });
+//Displays all reservations
+app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+  });
 
+app.get("/api/tables/:reservations", function(req, res) {
+    const chosen = req.params.reservations;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < reservations.length; i++) {
+      if (chosen === reservations[i].routeName) {
+        return res.json(reservations[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+
+  app.post("/api/reservations", function(req, res) {
+
+    const newTable = req.body;
+
+    
+    console.log(newTable);
+
+    reservations.push(newTable);
+
+    res.json(newTable);
+  });
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
+  //AJAX function that uses URL to reference API to GET 
